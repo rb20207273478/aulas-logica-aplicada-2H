79ba2a6a3803cba515e540fe399da8d80c077dda
rb20207273478 <rubensluiz1313@gmail.com>
2023-05-18 12:26:56 -0300

    Aula foi legal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         // Aula intrutória de Orientação à Objetos
-
-
+        Pessoa adao = new Pessoa();
+        //Declaração de objeto
+        Pessoa qualquer;
+        // Instanciação de objeto
+        qualquer = new Pessoa();
+        // Defefinição do formato da pessoa qualquer
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    // Atributos da classe
+    public String nome;
+    public String sobrenome;
+    // Métodos da classe
+    public void falar(){
+       System.out.println("falei");
+
+    }
+}
